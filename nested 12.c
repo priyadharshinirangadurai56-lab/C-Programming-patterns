98a1a903a25a10c32e77b64618a984cae21e3f8f
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-01-29 21:45:36 +0530

    Add files via upload
    nested 12.c

---
+++ nested 12.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    for (int i=1;i<=4;i++) {
+        for (int j=1;j<i+1;j++) {
+            printf("%d",j);
+        }
+        for (int j=1;j<=2*(4-i);j++) {
+            printf(" ");
+        }
+        for (int j=i;j>=1;j--) {
+            printf("%d",j);
+
+        }
+        printf("\n");
+    }
+    return 0;
+}
